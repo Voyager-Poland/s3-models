39f7f390ab53f9abb9615c0400ff3c9c82347309
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2025-02-06 08:25:09 +0100

    Event bus description

--- src/abstract/event-bus.ts
+++ src/abstract/event-bus.ts
@@ -1,14 +1,43 @@
 import { BehaviorSubject, Observable } from 'rxjs';
 
+/**
+ * Abstract class EventBus<T>
+ * 
+ * Provides a mechanism for emitting and subscribing to events of type T.
+ * Uses RxJS's BehaviorSubject to manage the event stream.
+ * 
+ * @template T - The type of events managed by the EventBus.
+ */
 export abstract class EventBus<T> {
   private eventSubject: BehaviorSubject<T>;
-	public event$: Observable<T>;
-	
+  private _event$: Observable<T>;
+
+  /**
+   * Constructor
+   * 
+   * Initializes the BehaviorSubject with an initial value and sets up the observable event stream.
+   * 
+   * @param initialValue - The initial value for the BehaviorSubject.
+   */
   protected constructor(initialValue: T) {
     this.eventSubject = new BehaviorSubject<T>(initialValue);
-		this.event$ = this.eventSubject.asObservable();
+    this._event$ = this.eventSubject.asObservable();
   }
 
+  /**
+   * Gets the observable event stream.
+   * 
+   * @returns Observable<T> - The observable event stream.
+   */
+  public get event$(): Observable<T> {
+    return this._event$;
+  }
+
+  /**
+   * Emits a new event.
+   * 
+   * @param event - The event to emit.
+   */
   emitEvent(event: T): void {
     this.eventSubject.next(event);
   }
--- src/index.ts
+++ src/index.ts
@@ -15,24 +15,4 @@ export { ProfileEventBusService } from './services/profile-bus.service';
 // Exporting abstract classes
 export { EventBus } from './abstract/event-bus';
 
-// Example usage of the exported components
-const profileToken = new ProfileTokenModel({ token: 'test-token', initials: 'TT', profilePictureUri: 'http://example.com/pic.jpg' });
 
-class TranslationService implements Translation {
-  translate(key: string, params?: any): string {
-    return key;
-  }
-  setLanguage(lang: string): void {
-    console.log(lang);
-  }
-  getLanguage(): string {
-    return 'en';
-  }
-}
-
-// // Example usage of the ProfileEventBusService
-// const profileEventBusService = new ProfileEventBusService();
-// profileEventBusService.event$.subscribe(profile => {
-//   console.log('Profile changed:', profile);
-// });
-// profileEventBusService.emitEvent(profileToken);
