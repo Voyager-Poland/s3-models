324bc4001177004fa3daa263e04b748d2467c92d
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2025-02-07 15:28:35 +0100

    Simplicitying

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-import { Translation } from './interfaces/translation';
-import { ProfileTokenModel } from './models/profile.token.model';
-import { ProfileEventBusService } from './services/profile-bus.service';
-import { EventBus } from './abstract/event-bus';
-import { LanguageEventBusService } from './services/language-bus.service';
-import { CurrencyEventBusService } from './services/currency-bus.service';
-
 // Exporting models
 export { ProfileTokenModel } from './models/profile.token.model';
 
