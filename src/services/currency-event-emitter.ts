8786ad36facb2881b38056f46dfb2c38b99be13a
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2025-02-11 10:28:36 +0100

    varsion 1.1.0

---
+++ src/services/currency-event-emitter.ts
@@ -0,0 +1,17 @@
+import { EventEmitter } from '../interfaces/event-emitter';
+import { CurrencyEventBusService } from './currency-bus.service';
+
+
+
+export class CurrencyEventEmitter implements EventEmitter<string> {
+
+	constructor(private service: CurrencyEventBusService) { }
+
+	public emitEvent(event: string): void {
+		this.service.emitEvent(event);
+	}
+
+	public get getCurrentValue(): string {
+		return this.service.getCurrentValue;
+	}
+}
---
+++ src/services/language-event-reader.ts
@@ -0,0 +1,12 @@
+import { Observable } from 'rxjs';
+import { EventReader } from '../interfaces/event-reader';
+import { LanguageEventBusService } from './language-bus.service';
+
+
+export class LanguageEventReader implements EventReader<string> {
+	constructor(private service: LanguageEventBusService) { }
+
+	public get event$(): Observable<string> {
+		return this.service.event$;
+	}
+}
