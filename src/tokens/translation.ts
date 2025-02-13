39bea5b46833e9b0eac430f09aaa269e5c2ef2c3
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2025-02-13 12:25:08 +0100

    Before Tralnsation_token

---
+++ src/tokens/translation.ts
@@ -0,0 +1,4 @@
+import { InjectionToken } from "@angular/core";
+import { Translation } from "../interfaces/translation";
+
+//export const TRANSLATION_TOKEN = new InjectionToken<Translation>('Translation');
