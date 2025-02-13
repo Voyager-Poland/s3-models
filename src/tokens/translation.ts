8f51b7c5610df4c459d13c05a75e9fb76896d388
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2025-02-13 20:56:08 +0100

    Before update jest

--- src/tokens/translation.ts
+++ src/tokens/translation.ts
@@ -1,10 +1,4 @@
 import { InjectionToken } from '@angular/core';
 import { Translation } from '../interfaces/translation';
 
-let injectionToken: InjectionToken<Translation>;
-export function TranslationToken(): InjectionToken<Translation> {
-  if (!injectionToken) {
-    injectionToken = new InjectionToken<Translation>('Translation');
-  }
-  return injectionToken;
-}
+export const TRANSLATION_TOKEN = new InjectionToken<Translation>('Translation');
