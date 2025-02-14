41f32d2a11f5a5519351cae46acab6461fede873
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2025-02-14 16:28:06 +0100

    1.3.1

--- src/services/state-saver.service.ts
+++ src/services/state-saver.service.ts
@@ -2,23 +2,26 @@ import { EventReader } from '../interfaces/event-reader';
 import { StateSaver } from '../interfaces/state-saver';
 import { Subscription } from 'rxjs';
 
-export class StateSaverService<T>  {
-  private subscription: Subscription;
+export class StateSaverService<T> {
+	private subscription: Subscription | undefined;
 
-  constructor(private store: StateSaver<T>, private reader: EventReader<T>) {
-    this.subscription = reader.event$.subscribe((state: T) => {
-      store.saveState(state);
-    })
-  }
+	constructor(private store: StateSaver<T>, private reader: EventReader<T>) {
+	}
 
-  // Method to unsubscribe from the event stream
-  unsubscribe(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
-  }
+	public start(): void {
+		this.subscription = this.reader.event$.subscribe((state: T) => {
+			this.store.saveState(state);
+		})
 
-  destroy(): void {
-    this.unsubscribe();
-  }
+	}
+	// Method to unsubscribe from the event stream
+	unsubscribe(): void {
+		if (this.subscription) {
+			this.subscription.unsubscribe();
+		}
+	}
+
+	destroy(): void {
+		this.unsubscribe();
+	}
 }
