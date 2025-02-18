7dced42d8d4e106374d1146b5cf5c86d9bfa7b92
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2025-02-18 14:48:52 +0100

    profile service

---
+++ src/services/context/profile.service.ts
@@ -0,0 +1,28 @@
+import { StateSaver } from "../../interfaces/state-saver";
+import { ProfileTokenModel } from "../../models/profile.token.model";
+import { ClientCallbackService } from "../client-callback.service";
+import { StateInitializerService } from "../state-initializer.service";
+import { StateComparisonService } from "../stores/state-comparison.service";
+
+export class ProfileService {
+
+	constructor(
+		private profileStateInitializer: StateInitializerService<ProfileTokenModel>,
+		private stateSaver: StateSaver<ProfileTokenModel>,
+		private clientCallbackService: ClientCallbackService,
+		private stateComparer: StateComparisonService<ProfileTokenModel>
+	) {
+		this.profileStateInitializer.setState();
+
+		this.clientCallbackService.executeIfBrowser(() => {
+			this.stateComparer.start();
+		});
+	}
+
+	destroy() {
+		this.clientCallbackService.executeIfBrowser(() => {
+			this.stateComparer.stop();
+		});
+	}
+
+}
