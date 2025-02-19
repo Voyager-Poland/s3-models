ee8c8de8728a8d49f0a1481ce96f3067429415ee
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2025-02-19 15:52:47 +0100

    index without alias

--- src/index.ts
+++ src/index.ts
@@ -2,43 +2,46 @@
 export { ProfileTokenModel } from './models/profile.token.model';
 
 // Exporting interfaces
-export { Translation } from '@interfaces/translation';
-export { EventEmitter } from '@interfaces/event-emitter';
-export { EventReader } from '@interfaces/event-reader';
-export { InitialStateProvider } from '@interfaces/internal-state.provider';
-export { Store } from '@interfaces/store';
-export { CookieOptions } from '@interfaces/cookie-options';
-export { ClientInfo } from '@interfaces/client-info';
+export { Translation } from './interfaces/translation';
+export { EventEmitter } from './interfaces/event-emitter';
+export { EventReader } from './interfaces/event-reader';
+export { InitialStateProvider } from './interfaces/internal-state.provider';
+export { Store } from './interfaces/store';
+export { CookieOptions } from './interfaces/cookie-options';
+export { ClientInfo } from './interfaces/client-info';
 
 // Exporting services
-export { StringEventBusService } from '@services/string-bus.service';
-export { StateSaverService } from '@services/state-saver.service';
-export { StateInitializerService } from '@services/state-initializer.service';
-export { ClientCallbackService } from '@services/client-callback.service';
+export { StringEventBusService } from './services/string-bus.service';
+export { StateSaverService } from './services/state-saver.service';
+export { StateInitializerService } from './services/state-initializer.service';
+export { ClientCallbackService } from './services/common/client-callback.service';
+export { ClientInfoService } from './services/common/client-info.service';
 
 // Exporting services/context
-export { ProfileEventBusService } from '@services/profile-bus.service';
-export { LanguageEventBusService } from '@services/language-bus.service';
-export { CurrencyEventBusService } from '@services/currency-bus.service';
-export { ProfileEventEmitter } from '@services/profile-event-emiter';
-export { ProfileEventReader } from '@services/profile-event-reader';
-export { CurrencyEventEmitter } from '@services/currency-event-emitter';
-export { CurrencyEventReader } from '@services/currency-event-reader';
-export { LanguageEventEmitter } from '@services/language-event-emitter';
-export { LanguageEventReader } from '@services/language-event-reader';
-export { LanguageTranslationService } from '@services/language-translation.service';
-export { ProfileTokenValue } from '@services/profile-token-value';
-export { ProfileService } from '@services/profile.service';
+export { ProfileEventBusService } from './services/context/profile-bus.service';
+export { LanguageEventBusService } from './services/context/language-bus.service';
+export { CurrencyEventBusService } from './services/context/currency-bus.service';
+export { ProfileEventEmitter } from './services/context/profile-event-emiter';
+export { ProfileEventReader } from './services/context/profile-event-reader';
+export { CurrencyEventEmitter } from './services/context/currency-event-emitter';
+export { CurrencyEventReader } from './services/context/currency-event-reader';
+export { LanguageEventEmitter } from './services/context/language-event-emitter';
+export { LanguageEventReader } from './services/context/language-event-reader';
+export { LanguageTranslationService } from './services/context/language-translation.service';
+export { ProfileTokenValue } from './services/profile-token-value';
+export { ProfileService } from './services/context/profile.service';
 
 // Exporting store services
-export { CookieStore } from '@services/cookie-store';
-export { SSRStore } from '@services/ssr-store';
-export { ProfileSSRStore } from '@services/profile-ssr-store';
-export { OptionsProcessor } from '@services/options-processor';
-export { ProfileTokenCookieStore } from '@services/profile-cookie-store';
-export { IntialStateService as StateManagementService } from '@services/initiall-state.service';
-export { StateComparisonService } from '@services/state-comparison.service';
+export { CookieStore } from './services/stores/cookie-store';
+export { SSRStore } from './services/stores/ssr-store';
+export { ProfileSSRStore } from './services/stores/profile-ssr-store';
+export { OptionsProcessor } from './services/stores/options-processor';
+export { ProfileTokenCookieStore } from './services/stores/profile-cookie-store';
+export { IntialStateService as StateManagementService } from './services/stores/initiall-state.service';
+export { StateComparisonService } from './services/stores/state-comparison.service';
+
 // Exporting abstract classes
 export { EventBus } from './abstract/event-bus';
 
-export { ClientModule } from '@services/client.module';
+// Exporting modules
+export { ClientModule } from './services/common/client.module';
