986ed12a9b52cb66c34ddcf4ce08d49bd438b8c6
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2025-02-19 23:44:28 +0100

    reginstation fix

--- src/services/common/client.module.ts
+++ src/services/common/client.module.ts
@@ -1,12 +1,21 @@
-import { NgModule } from '@angular/core';
-import { ClientCallbackService } from './client-callback.service';
-import { ClientInfoService } from './client-info.service';
-import { CLIENT_INFO_TOKEN } from '../../tokens/tokens';
+//npm test -- dist/services/common/client.module.spec.js                    
+import { Inject, inject, NgModule, PLATFORM_ID } from "@angular/core";
+import { ClientCallbackService } from "./client-callback.service";
+import { ClientInfoService } from "./client-info.service";
+import { CLIENT_INFO_TOKEN } from "../../tokens/tokens";
+import { ClientInfo } from "../../interfaces/client-info";
 
 @NgModule({
-	providers: [
-		ClientCallbackService,
-		{ provide: CLIENT_INFO_TOKEN, useClass: ClientInfoService }
-	]
+  providers: [
+    { provide: ClientCallbackService, useFactory: (client: ClientInfo ) => new ClientCallbackService(client), deps: [CLIENT_INFO_TOKEN] },
+    {
+      provide: CLIENT_INFO_TOKEN,
+      useFactory: () => {
+        const platform = inject(PLATFORM_ID);
+        return new ClientInfoService(platform);
+      },
+      deps: [PLATFORM_ID],
+    },
+  ],
 })
-export class ClientModule { }
+export class ClientModule {}
