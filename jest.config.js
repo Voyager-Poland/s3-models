ea6876613f4b64d6c2b1aed8f07b7fbacbd790e6
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2025-02-19 08:59:35 +0100

    type=module

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,12 @@ module.exports = {
 	testEnvironment: 'jsdom',
 	transform: {
 		'^.+\\.(ts|html)$': 'ts-jest',
+		'^.+\\.mjs$': 'babel-jest', // Add this line to handle ES modules
 	},
-	moduleFileExtensions: ['ts', 'html', 'js', 'json'],
-	transformIgnorePatterns: ['<rootDir>/node_modules/'],
+	moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
+	transformIgnorePatterns: [
+		'node_modules/(?!@angular|rxjs)', // Add this line to handle ES modules in node_modules
+	],
 	globals: {
 		'ts-jest': {
 			tsconfig: '<rootDir>/tsconfig.spec.json',
