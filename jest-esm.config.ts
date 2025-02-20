c0e83235307c63fe3fb44e2dec9e9660ee0279f5
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2025-02-20 06:44:21 +0100

    problem z clientem

--- jest-esm.config.ts
+++ jest-esm.config.ts
@@ -12,6 +12,8 @@ export default {
         ...esmPreset.moduleNameMapper,
         ...pathsToModuleNameMapper(tsconfig.compilerOptions.paths, { prefix: '<rootDir>' }),
         '^rxjs': '<rootDir>/node_modules/rxjs/dist/bundles/rxjs.umd.js',
+        '^@angular/core/testing$': '<rootDir>/node_modules/@angular/core/fesm2022/testing.mjs',
+        '^@angular/common$': '<rootDir>/node_modules/@angular/common/fesm2022/common.mjs',
     },
     setupFilesAfterEnv: ['<rootDir>/setup-jest-esm.ts'],
     transform: {
@@ -24,4 +26,5 @@ export default {
             },
         ],
     },
+    extensionsToTreatAsEsm: ['.ts'],
 } satisfies JestConfigWithTsJest;
