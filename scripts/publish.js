743beb0f3dd47d84910f10dc07f7a5a6ff1577a9
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2025-02-22 23:47:43 +0100

    cd to distr

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,5 +1,4 @@
 const { execSync } = require('child_process');
-const fs = require('fs');
 const path = require('path');
 
 const distPath = path.join(__dirname, '../dist');
@@ -10,17 +9,8 @@ if (!npmToken) {
   process.exit(1);
 }
 
-console.log('Copying package.json from dist to root');
-const packageJsonSrc = path.join(distPath, 'package.json');
-const packageJsonDest = path.join(__dirname, '..', 'package.json');
-fs.copyFileSync(packageJsonSrc, packageJsonDest);
-
-console.log('Moving contents of dist to root');
-fs.readdirSync(distPath).forEach(file => {
-  const srcPath = path.join(distPath, file);
-  const destPath = path.join(__dirname, '..', file);
-  fs.renameSync(srcPath, destPath);
-});
+console.log('Changing directory to dist');
+process.chdir(distPath);
 
 console.log('Publishing package to npm');
 execSync('npm publish --access public', {
