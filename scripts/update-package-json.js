deae6f81f5ccf6fd33322577c4514b5d54a6b4ee
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2025-02-22 21:54:39 +0100

    removed files opperation

--- scripts/update-package-json.js
+++ scripts/update-package-json.js
@@ -1,7 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-const { updatePackageJson } = require('./update-package-json-functions');
-const { moveDir, copyFiles } = require('./file-operations');
 
 const distPath = path.join(__dirname, '../dist');
 const packageJsonPath = path.join(distPath, 'package.json');
@@ -12,38 +10,42 @@ console.log('distPath:', distPath);
 console.log('packageJsonPath:', packageJsonPath);
 
 // Update package.json
-updatePackageJson(packageJsonPath, packageJson);
+const updatedPackageJson = {
+  ...packageJson,
+  files: [
+    "fesm2022",
+    "lib",
+    "LICENSE",
+    "README.md",
+    "index.d.ts",
+    "public-api.d.ts"
+  ]
+};
 
-// Ensure directories exist before moving
-const fesm2022Src = path.join(distPath, 'fesm2022');
-const libSrc = path.join(distPath, 'lib');
-console.log('Checking if directories exist before moving:');
-console.log('fesm2022:', fs.existsSync(fesm2022Src));
-console.log('lib:', fs.existsSync(libSrc));
-
-// Log contents of dist directory
-console.log('Contents of dist directory before moving:');
-console.log(fs.readdirSync(distPath));
+fs.writeFileSync(packageJsonPath, JSON.stringify(updatedPackageJson, null, 2));
+console.log('package.json updated successfully');
 
 // Move directories to root of dist
-moveDir(fesm2022Src, fesm2022Src);
-moveDir(libSrc, libSrc);
+const moveDir = (src, dest) => {
+  if (fs.existsSync(src)) {
+    fs.renameSync(src, dest);
+    console.log(`Moved directory from ${src} to ${dest}`);
+  } else {
+    console.log(`Source directory ${src} does not exist`);
+  }
+};
 
-// Ensure directories exist after moving
-console.log('Checking if directories exist after moving:');
-console.log('fesm2022:', fs.existsSync(fesm2022Src));
-console.log('lib:', fs.existsSync(libSrc));
-
-// Log contents of dist directory
-console.log('Contents of dist directory after moving:');
-console.log(fs.readdirSync(distPath));
+moveDir(path.join(distPath, 'dist/fesm2022'), path.join(distPath, 'fesm2022'));
+moveDir(path.join(distPath, 'dist/lib'), path.join(distPath, 'lib'));
 
 // Copy LICENSE, README.md, index.d.ts, and public-api.d.ts to dist
 const filesToCopy = ['LICENSE', 'README.md', 'index.d.ts', 'public-api.d.ts'];
 filesToCopy.forEach(file => {
-  const srcPath = path.join(__dirname, file);
-  if (fs.existsSync(srcPath)) {
-    copyFiles([file], __dirname, distPath);
+  const src = path.join(__dirname, '..', file);
+  const dest = path.join(distPath, file);
+  if (fs.existsSync(src)) {
+    fs.copyFileSync(src, dest);
+    console.log(`Copied file from ${src} to ${dest}`);
   } else {
     console.warn(`File ${file} does not exist and will not be copied.`);
   }
