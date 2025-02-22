8264c3f8e0a0937018715515b6d1aebfdc24a9a2
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2025-02-22 07:19:46 +0100

    profil-event.module

---
+++ src/lib/context/profile-event.module.spec.ts
@@ -0,0 +1,28 @@
+import { TestBed } from '@angular/core/testing';
+import { ProfileEventModule } from './profile-event.module';
+import { ProfileEventReader } from './profile-event-reader';
+import { ProfileEventBusService } from './profile-bus.service';
+import { ProfileEventEmitter } from './profile-event-emitter';
+
+describe('ProfileEventModule', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [ProfileEventModule]
+        });
+    });
+
+    it('should provide ProfileEventReader', () => {
+        const service = TestBed.inject(ProfileEventReader);
+        expect(service).toBeTruthy();
+    });
+
+    it('should provide ProfileEventBusService', () => {
+        const service = TestBed.inject(ProfileEventBusService);
+        expect(service).toBeTruthy();
+    });
+
+    it('should provide ProfileEventEmitter', () => {
+        const service = TestBed.inject(ProfileEventEmitter);
+        expect(service).toBeTruthy();
+    });
+});
---
+++ src/lib/context/profile-event.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { ProfileEventReader } from './profile-event-reader';
+import { ProfileEventBusService } from './profile-bus.service';
+import { ProfileEventEmitter } from './profile-event-emitter';
+
+export function createProfileEventReader(service: ProfileEventBusService): ProfileEventReader {
+    return new ProfileEventReader(service);
+}
+
+export function createProfileEventEmitter(service: ProfileEventBusService): ProfileEventEmitter {
+    return new ProfileEventEmitter(service);
+}
+
+@NgModule({
+    providers: [
+        ProfileEventBusService,
+        {
+            provide: ProfileEventReader,
+            useFactory: createProfileEventReader,
+            deps: [ProfileEventBusService]
+        },
+        {
+            provide: ProfileEventEmitter,
+            useFactory: createProfileEventEmitter,
+            deps: [ProfileEventBusService]
+        }
+    ]
+})
+export class ProfileEventModule { }
