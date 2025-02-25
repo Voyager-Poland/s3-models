263b82e1b4c4f2373264c36e6e7ed89a67a508c9
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2025-02-25 15:41:07 +0100

    modified:   src/lib/context/profile-event.module.spec.ts

--- src/lib/context/profile-event.module.spec.ts
+++ src/lib/context/profile-event.module.spec.ts
@@ -5,24 +5,30 @@ import { ProfileEventBusService } from './profile-bus.service';
 import { ProfileEventEmitter } from './profile-event-emitter';
 
 describe('ProfileEventModule', () => {
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            imports: [ProfileEventModule]
-        });
-    });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			imports: [ProfileEventModule]
+		});
+	});
 
-    it('should provide ProfileEventReader', () => {
-        const service = TestBed.inject(ProfileEventReader);
-        expect(service).toBeTruthy();
-    });
+	it('should provide ProfileEventReader', () => {
+		const service = TestBed.inject(ProfileEventReader);
+		expect(service).toBeTruthy();
+	});
 
-    it('should provide ProfileEventBusService', () => {
-        const service = TestBed.inject(ProfileEventBusService);
-        expect(service).toBeTruthy();
-    });
+	it('should provide ProfileEventBusService', () => {
+		const service = TestBed.inject(ProfileEventBusService);
+		expect(service).toBeTruthy();
+	});
 
-    it('should provide ProfileEventEmitter', () => {
-        const service = TestBed.inject(ProfileEventEmitter);
-        expect(service).toBeTruthy();
-    });
+	it('should provide ProfileEventEmitter', () => {
+		const service = TestBed.inject(ProfileEventEmitter);
+		expect(service).toBeTruthy();
+	});
+
+	it('should provide the same instance of ProfileEventBusService', () => {
+		const service1 = TestBed.inject(ProfileEventBusService);
+		const service2 = TestBed.inject(ProfileEventBusService);
+		expect(service1).toBe(service2);
+	});
 });
