a7cbb5cbf07bf3f6d953855d15816223b7eeddb9
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2025-02-25 15:52:41 +0100

    multi to  false

--- src/lib/context/profile-event.module.ts
+++ src/lib/context/profile-event.module.ts
@@ -4,26 +4,27 @@ import { ProfileEventBusService } from './profile-bus.service';
 import { ProfileEventEmitter } from './profile-event-emitter';
 
 export function createProfileEventReader(service: ProfileEventBusService): ProfileEventReader {
-    return new ProfileEventReader(service);
+	return new ProfileEventReader(service);
 }
 
 export function createProfileEventEmitter(service: ProfileEventBusService): ProfileEventEmitter {
-    return new ProfileEventEmitter(service);
+	return new ProfileEventEmitter(service);
 }
 
 @NgModule({
-    providers: [
-        ProfileEventBusService,
-        {
-            provide: ProfileEventReader,
-            useFactory: createProfileEventReader,
-            deps: [ProfileEventBusService]
-        },
-        {
-            provide: ProfileEventEmitter,
-            useFactory: createProfileEventEmitter,
-            deps: [ProfileEventBusService]
-        }
-    ]
+	providers: [
+		{ provide: ProfileEventBusService, useClass: ProfileEventBusService, multi: false },
+		{
+			provide: ProfileEventReader,
+			useFactory: createProfileEventReader,
+			deps: [ProfileEventBusService],
+
+		},
+		{
+			provide: ProfileEventEmitter,
+			useFactory: createProfileEventEmitter,
+			deps: [ProfileEventBusService]
+		}
+	]
 })
 export class ProfileEventModule { }
