cea4fd1fea8cd24c6edd8c745dbcb459d5724893
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2025-02-25 15:27:39 +0100

    atribute Injectable

--- src/lib/context/profile-bus.service.ts
+++ src/lib/context/profile-bus.service.ts
@@ -2,18 +2,24 @@ import { EventBus } from '../abstract/event-bus';
 import { EventReader } from '../interfaces/event-reader';
 import { EventEmitter } from '../interfaces/event-emitter';
 import { ProfileTokenModel } from '../models/profile.token.model';
+import { Injectable } from '@angular/core';
 
 /**
  * Service for managing profile events.
  * 
  * Extends the EventBus class to handle events of type ProfileTokenModel.
  */
+Injectable(
+	{
+		providedIn: 'root'
+	}
+)
 export class ProfileEventBusService extends EventBus<ProfileTokenModel>
 	implements EventReader<ProfileTokenModel>, EventEmitter<ProfileTokenModel> {
 	/**
 	 * Creates an instance of ProfileEventBusService.
 	 * Initializes the EventBus with an empty ProfileTokenModel.
-	 	 */
+			 */
 	constructor() {
 		super(ProfileTokenModel.createEmpty());
 	}
